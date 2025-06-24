24b3f3711a5bb6a38eca1a482a4c21c3fee7cad3
Remenby31 <baptiste.cruvellier31@gmail.com>
2025-06-24 12:14:12 +0200

    remove dist

--- dist/core/Config.d.ts
+++
@@ -1,8 +0,0 @@
-import { AgentTreeConfig } from '../types';
-export declare class Config {
-    private static defaultConfig;
-    static merge(userConfig?: Partial<AgentTreeConfig>): AgentTreeConfig;
-    static validate(config: AgentTreeConfig): void;
-    static getDefault(): AgentTreeConfig;
-}
-//# sourceMappingURL=Config.d.ts.map
